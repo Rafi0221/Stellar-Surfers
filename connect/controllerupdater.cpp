6091d701ef6f59de2469e4d347d2f509fdee0716
Rafi0221 <rafal.pyzik@student.uj.edu.pl>
2023-05-27 23:32:17 +0200

    communication fix 2

--- connect/controllerupdater.cpp
+++ connect/controllerupdater.cpp
@@ -23,11 +23,13 @@ void ControllerUpdater::update(QVector3D position, float speed, int collision) {
     float x = position.x();
     float y = position.y();
     float z = position.z();
+    float hash = x + y + z + speed + collision;
     buffer.append(reinterpret_cast<const char*>(&x), sizeof(x));
     buffer.append(reinterpret_cast<const char*>(&y), sizeof(y));
     buffer.append(reinterpret_cast<const char*>(&z), sizeof(z));
     buffer.append(reinterpret_cast<const char*>(&speed), sizeof(speed));
     buffer.append(reinterpret_cast<const char*>(&collision), sizeof(collision));
+    buffer.append(reinterpret_cast<const char*>(&hash), sizeof(hash));
 
     connectManager->send(buffer);
 }
